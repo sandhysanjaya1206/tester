18fd122616f78e8b6162512772d603cd1e9281f6
Sandhy Sanjaya <sandhysanjaya1202@gmail.com>
2020-07-10 16:12:30 +0700

    rambah

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ const buildShareContent = function(data) {
         '<meta property="fb:app_id" content="985489241643811" />' +
         '<meta property="al:android:package" content="com.meeber.meeberian" />' +
         '<meta property="al:android:app_name" content="Meeberian" />' +
-        '<meta property="al:android:url" content="meeber" />' +
-        '<meta property="al:ios:url" content="meeber" />' +
+        '<meta property="al:android:url" content="meeber://meeberian.com" />' +
+        '<meta property="al:ios:url" content="meeber://meeberian.com" />' +
         '<meta property="al:ios:app_store_id" content="1459505870" />' +
         '<meta property="al:ios:app_name" content="Meeberian" />' +
         '<meta property="al:web:should_fallback" content="true" />' +
